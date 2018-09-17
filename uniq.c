86f94315562068009c84dba3d9b6eeec909dc67b
buszk <17345356+buszk@users.noreply.github.com>
2018-09-17 00:55:58 -0400

    switch to linebuffer for dynamic line size

--- uniq.c
+++ uniq.c
@@ -2,17 +2,26 @@
 #include "stat.h"
 #include "user.h"
 
-char line[512];
-char prev_line[512];
-char buf[512];
+#define BUFSIZE 3
 
+struct linebuffer {
+	uint size;		// Allocated
+	uint length;	// used
+	char *buffer;
+};
+
+char buf[BUFSIZE];
+struct linebuffer prev_line;
+struct linebuffer line;
+
+// C str functions
 char* strncpy(char*s, char*t, int n) {
 	char *os;
 	
 	os = s;
 	if (n <= 0)
 		return os;
-	while(--n > 0 && (*s++ = *t++) != 0)
+	while(n-- > 0 && (*s++ = *t++) != 0)
 		;
 	return os;
 }
@@ -37,46 +46,105 @@ int strncmp(const char*p, const char*q, uint n) {
 	return (uchar)*p - (uchar)*q;
 }
 
+// linebuffer function
+void init_linebuffer (struct linebuffer *linebuffer) {
+	linebuffer->size = BUFSIZE;
+	linebuffer->length = 0;
+	linebuffer->buffer = malloc(BUFSIZE);
+}
+
+void linebuffer_print(struct linebuffer *linebuffer) {
+	//printf(1, linebuffer->buffer, linebuffer->length);
+	int i;
+	//write(1, "---", 3);
+	for (i =  0; i < linebuffer->length; i++ ) write(1, linebuffer->buffer+i, 1);
+	//write(1, "\n", 1);
+}
+
+void double_linebuffer (struct linebuffer *linebuffer) {
+	char *tmp = linebuffer->buffer;
+	linebuffer->buffer = malloc(linebuffer->size*2);
+	linebuffer->size = linebuffer->size * 2;
+	memset(linebuffer->buffer, 0, linebuffer->size);
+	strncpy(linebuffer->buffer, tmp, linebuffer->length);
+	free((void*) tmp);
+}
+
+void append_to_linebuffer(struct linebuffer *linebuffer, char* str, int len) {
+	// Make sure enough space
+	while(linebuffer->size <=linebuffer->length + len)  {
+		double_linebuffer(linebuffer);
+	}
+	strncpy(linebuffer->buffer + linebuffer->length, str, len);
+	linebuffer->length += len;
+}
+
+int linebuffer_cmp(struct linebuffer *linebuffer1, struct linebuffer *linebuffer2 ) {
+	if (linebuffer1->length != linebuffer2->length) 
+		return linebuffer1->length - linebuffer2->length;
+	return strncmp(linebuffer1->buffer, linebuffer2->buffer, linebuffer1->length);
+}
+
+
+
 void uniq(int fd) {
-	int n, i; //n: count of read input, i: for loop counter
+	int i, n;
 	int line_start; //tracks the current line
 	int line_size;
 	int line_ind;
 	int first_line = 1;
 	line_start = line_size = line_ind = 0;
-	while((n = read(fd, buf, sizeof(buf))) > 0) {
+	// Init linebuffers
+	init_linebuffer(&prev_line);
+	init_linebuffer(&line);
+
+	while((n = read(fd, buf, sizeof(buf)-1)) > 0) {
+		buf[sizeof(buf)-1] = 0;
+		//printf(2, "read buf %s\n", buf);
 		line_start = 0;
+		line_size = 0;
 		for (i = 0; i < n; i++ ) {
 			if (line_size == 0) {
 				line_start = i;
 			}
 			line_size++;
 			if (buf[i] == '\n' || buf[i] == '\r') {// hit new line
-				safestrcpy(line + line_ind, buf + line_start, ++line_size); //copy to line buffer
-				line_ind = 0;
-				if (strncmp(line, prev_line, line_size) != 0 ) {//uniq line
+				//printf(2, "CurrentLine:%s\n", line.buffer);
+				append_to_linebuffer(&line, buf + line_start, i -line_start+1);
+				//printf(2, "CurrentLine:%s\n", line.buffer);
+				//printf(2, "PrevLine:%s\n", prev_line.buffer);
+				if (linebuffer_cmp(&line, &prev_line) != 0 ) {//uniq line
+					//printf(2, "Uniq line\n");
 					if (first_line == 0) {
-						printf(1, prev_line, sizeof(prev_line));
+						linebuffer_print(&prev_line);
 					} else {
 						first_line = 0;
 					}
-					safestrcpy(prev_line, line, line_size); // copy to prev line buffer
-					line_size = 0;
+					free((void*)prev_line.buffer);
+					prev_line.size = line.size;
+					prev_line.length = line.length;
+					prev_line.buffer = line.buffer;
+					init_linebuffer(&line);
 				}
 				else {
-					printf(2, "Hit same line\n", 15);
+					//printf(2, "Hit same line\n");
+					init_linebuffer(&line);
 				}
+				line_size = 0;
+				if (i == n-1) continue;
 
 			}
 			if (i == n-1) {// last char of the line, copy remaining buf over to line
-				strncpy(line, buf + line_start, line_size);
-				line_ind = line_size;
+				//printf(2, "OldLine:%s\n", line.buffer);
+				append_to_linebuffer(&line, buf + line_start, sizeof(buf) - line_start -1);
+				//printf(2, "OldLine:%s\n", line.buffer);
 			}
 
 		}
 		
 	}
-	printf(1, line, sizeof(line));
+	//printf(2, "LastLine\n");
+	linebuffer_print(&prev_line);
 }
 
 
